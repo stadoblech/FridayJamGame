ec580684e311d1ae4d64d54aed81eac6fad551c0
Bohdan Taraba <tbohdan@seznam.cz>
2015-10-10 00:24:12 +0200

    kamera
    nasleduje hrace

---
+++ FridayGameJam/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform Player;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Player.position.x,Player.position.y, -10);
+	}
+}
