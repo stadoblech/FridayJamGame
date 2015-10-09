d25480040949c71732b3cfb69c6e8347ea2b15d3
Bohdan Taraba <tbohdan@seznam.cz>
2015-10-09 23:44:32 +0200

    cesty pro nepritele

---
+++ FridayGameJam/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 0.5f;
+    private GameObject path;
+    private int nextCheckpoint = 1;
+    private Transform nextPoint;
+
+    void Start () {
+        path = PathPickerScript.GetPath();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (Transform t in path.transform)
+        {
+            if (t.name == nextCheckpoint.ToString())
+            {
+                transform.position = Vector2.MoveTowards(transform.position,t.position,speed * Time.deltaTime);
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "checkpoint")
+        {
+            nextCheckpoint++;
+        }
+        
+        if (coll.tag == "end")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ FridayGameJam/Assets/Scripts/PathPickerScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathPickerScript : MonoBehaviour {
+
+    public List<GameObject> Paths;
+    public float respawnCooldown = 0.5f;
+    public GameObject enemy;
+    
+    private static GameObject pickedPath;
+
+
+    
+    private float respawnTimer;
+
+    // Use this for initialization
+	void Start () {
+        respawnTimer = respawnCooldown;
+        pickedPath = Paths[Random.Range(0,Paths.Count)];
+        
+        foreach (GameObject o in Paths)
+        {
+            o.active = false;
+            pickedPath.active = true;
+        }
+        createEnemy();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (respawnTimer <= 0)
+        {
+            respawnTimer = respawnCooldown;
+            createEnemy();
+        }
+
+        respawnTimer -= Time.deltaTime;
+	}
+
+    public static GameObject GetPath()
+    {
+        
+        return pickedPath;
+    }
+
+    void createEnemy()
+    {
+        GameObject start = GetPath().transform.Find("start").gameObject;
+        Instantiate(enemy,start.transform.position,Quaternion.identity);
+    }
+}
