735eb27056aa992027ed5a96b13068fc48ddc9a8
Bohdan Taraba <tbohdan@seznam.cz>
2015-10-10 02:23:04 +0200

    staveni veze

--- FridayGameJam/Assets/Scripts/PlayerMovement.cs
+++ FridayGameJam/Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,10 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        if (GetComponent<Rigidbody2D>().isKinematic)
+        {
+            speed *= 10;
+        }
 	}
 	
 	// Update is called once per frame
@@ -21,26 +24,33 @@ public class PlayerMovement : MonoBehaviour {
 
     private void playerMovement()
     {
-        Vector2 vel = GetComponent<Rigidbody2D>().velocity;
+        if (!GetComponent<Rigidbody2D>().isKinematic)
+        {
+            Vector2 vel = GetComponent<Rigidbody2D>().velocity;
 
-        if (Input.GetKey(UpMovement))
-        {
-            vel.y += speed;
-        }
-        if (Input.GetKey(downMovement))
-        {
-            vel.y -= speed;
-        }
-        if (Input.GetKey(RighMovement))
-        {
-            vel.x += speed;
-        }
-        if (Input.GetKey(LeftMovement))
-        {
-            vel.x -= speed;
-        }
+            if (Input.GetKey(UpMovement))
+            {
+                vel.y += speed;
+            }
+            if (Input.GetKey(downMovement))
+            {
+                vel.y -= speed;
+            }
+            if (Input.GetKey(RighMovement))
+            {
+                vel.x += speed;
+            }
+            if (Input.GetKey(LeftMovement))
+            {
+                vel.x -= speed;
+            }
 
-        GetComponent<Rigidbody2D>().velocity = vel;
+            GetComponent<Rigidbody2D>().velocity = vel;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+        }
     }
 
     
---
+++ FridayGameJam/Assets/Scripts/TowerDeploymentScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerDeploymentScript : MonoBehaviour {
+
+    public KeyCode DeployTower = KeyCode.Space;
+    public GameObject tower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(DeployTower))
+        {
+            Instantiate(tower,transform.position,Quaternion.identity);
+        }
+	}
+}
