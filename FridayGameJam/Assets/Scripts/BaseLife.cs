c3c42ab3843258102e625f5131e5496b657186fd
Dzarin <jarin97@seznam.cz>
2015-10-10 19:43:59 +0200

    Bas

---
+++ FridayGameJam/Assets/Scripts/BaseLife.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseLife : MonoBehaviour {
+
+    public int lives = 20;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (lives <= 0)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+    if (coll.tag == "Enemy")
+    {
+        lives--;
+    }
+
+    }
+}
